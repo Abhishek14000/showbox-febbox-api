2f77971db8dc6c5467f4cff14608edc5a5bfa6e3
Abhishek Singh <singhabhishek1400@gmail.com>
2026-07-06 20:52:01 +0530

    Update render_start.sh

--- render_start.sh
+++ render_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Start the Python Bypass Server in the background
-cd bypass && python3 server.py &
 
-# Start the Node.js API in the foreground (Render needs this to stay running)
-cd ../api && npm start
+# Start the Python Bypass Server in a subshell (so it doesn't move our main directory)
+(cd bypass && python3 server.py) &
+
+# Now we are still in the root directory, so we can just cd into api
+cd api && npm start
